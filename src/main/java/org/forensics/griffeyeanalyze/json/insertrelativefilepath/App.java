48d940500ce9eb2a9a0e081c0d68a47e4880f5e7
Garrett Groce <ggroce@gmail.com>
2020-12-19 20:39:28 -0600

    Added output to file.

--- src/main/java/org/forensics/griffeyeanalyze/json/insertrelativefilepath/App.java
+++ src/main/java/org/forensics/griffeyeanalyze/json/insertrelativefilepath/App.java
@@ -3,6 +3,7 @@ package org.forensics.griffeyeanalyze.json.insertrelativefilepath;
 import javax.json.Json;
 import javax.json.stream.JsonParser;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
@@ -17,6 +18,7 @@ public class App
 
 //        try (InputStream in = url.openStream(); JsonParser parser = Json.createParser(in)) {
         try (FileInputStream is = new FileInputStream("C:\\report\\export.json");
+             FileWriter outputFile = new FileWriter("C:\\report\\output.json");
              JsonParser parser = factory.createParser(is, StandardCharsets.UTF_8)) {
 
             JsonParser.Event previousEvent = null;
@@ -26,22 +28,28 @@ public class App
                 switch (event) {
                     case START_OBJECT:
                         if (previousEvent == JsonParser.Event.END_OBJECT) {
+                            outputFile.write(",");
+                            outputFile.write("{");
                             System.out.println(",");
                             System.out.println("{");
                         } else {
+                            outputFile.write("{");
                             System.out.println("{");
                         }
                         previousEvent = JsonParser.Event.START_OBJECT;
                         break;
                     case END_OBJECT:
+                        outputFile.write("}");
                         System.out.print("}");
                         previousEvent = JsonParser.Event.END_OBJECT;
                         break;
                     case START_ARRAY:
+                        outputFile.write("[");
                         System.out.println("[");
                         previousEvent = JsonParser.Event.START_ARRAY;
                         break;
                     case END_ARRAY:
+                        outputFile.write("]");
                         System.out.println("]");
                         previousEvent = JsonParser.Event.END_ARRAY;
                         break;
@@ -55,9 +63,11 @@ public class App
                             case VALUE_TRUE:
                             case VALUE_FALSE:
                             case VALUE_NULL:
+                                outputFile.write("," + "\"" + key + "\"" + ":");
                                 System.out.print("," + "\"" + key + "\"" + ":");
                                 break;
                             default:
+                                outputFile.write("\"" + key + "\"" + ":");
                                 System.out.print("\"" + key + "\"" + ":");
                         }
                         previousEvent = JsonParser.Event.KEY_NAME;
@@ -65,37 +75,45 @@ public class App
                         break;
                     case VALUE_STRING:
                         String string = parser.getString().replace("\\","\\\\");
+                        outputFile.write("\"" + string + "\"");
                         System.out.print("\"" + string + "\"");
                         previousEvent = JsonParser.Event.VALUE_STRING;
                         if (previousKey != null && previousKey.equals("MD5")) {
                             md5 = string;
                         }
                         if (previousKey != null && previousKey.equals("MediaSize")) {
+                            outputFile.write(",\"RelativeFilePath\":\"Files\\\\" + md5 + "\"");
                             System.out.print(",\"RelativeFilePath\":\"Files\\\\" + md5 + "\"");
                         }
                         break;
                     case VALUE_NUMBER:
                         BigDecimal number = parser.getBigDecimal();
+                        outputFile.write(number.toString());
                         System.out.print(number);
                         if (previousKey != null && previousKey.equals("MediaSize")) {
+                            outputFile.write(",\"RelativeFilePath\":\"Files\\\\" + md5 + "\"");
                             System.out.print(",\"RelativeFilePath\":\"Files\\\\" + md5 + "\"");
                         }
                         previousEvent = JsonParser.Event.VALUE_NUMBER;
                         break;
                     case VALUE_TRUE:
+                        outputFile.write("true");
                         System.out.print(true);
                         previousEvent = JsonParser.Event.VALUE_TRUE;
                         break;
                     case VALUE_FALSE:
+                        outputFile.write("false");
                         System.out.print(false);
                         previousEvent = JsonParser.Event.VALUE_FALSE;
                         break;
                     case VALUE_NULL:
+                        outputFile.write("");
                         System.out.print("");
                         previousEvent = JsonParser.Event.VALUE_NULL;
                         break;
                 }
             }
+            outputFile.close();
             parser.close();
         }
     }
